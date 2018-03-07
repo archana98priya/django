5a9473ddb8c8209570e6db2805fbe14090fedffe
Archana Priyadarshani <archana98priya@gmail.com>
2018-03-07 14:15:52 +0530

    article blog

---
+++ django_project/mysite1/assets/slugify.js
@@ -0,0 +1,14 @@
+const titleInput = document.querySelector('input[name=title]');
+const slugInput= document.querySelector('input[name=title]');
+
+const slugify = (val) => {
+  return val.toString().toLowerCase().trim()
+  .replace(/&/g,'-and-')  // replace & with '-and-'
+
+  .replace(/[\s\W-]+/g,'-') // replaces spaces,non word chars and dashes with single dash
+};
+
+titleInput.addEventListener('keyup',(e)=>{
+    slugInput.setAttribute('value',slugify(titleInput.value));
+
+});
